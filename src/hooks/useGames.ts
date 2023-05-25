cfa7ddda9bbc152472f3643b7893bae3810a8b7c
Joseph Jack <joseph.w.jack@gmail.com>
2023-05-25 13:42:35 -0700

    implement infinite queries

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,5 @@
 
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
@@ -18,21 +18,29 @@ export interface Game {
 
 
 const useGames = (gameQuery: GameQuery) =>
-  useQuery<FetchResponse<Game>, Error>({
+  useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ['games', gameQuery],
-    queryFn: () =>
+    queryFn: ({ pageParam = 1 }) =>
       apiClient
         .getAll({
           params: { 
             genres: gameQuery.genre?.id, 
             parent_platforms: gameQuery.platform?.id,
             ordering: gameQuery.sortOrder,
-            search: gameQuery.searchText
+            search: gameQuery.searchText,
+            page: pageParam
           },
         }),
+        getNextPageParam: ( lastPage, allPages) => {
+          return lastPage.next ? allPages.length + 1 : undefined;
+        }
     });
         
 
   
 
 export default useGames;
+
+function useInfiniteQueries<T, U>(arg0: { queryKey: (string | GameQuery)[]; queryFn: () => Promise<FetchResponse<Game>>; }) {
+  throw new Error("Function not implemented.");
+}
